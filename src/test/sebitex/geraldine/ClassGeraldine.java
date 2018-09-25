8cc51eeec209fd4dfabf11efd31c44e70ea98552
joseantoniohp <joseantoniohp25@hotmail.com>
2018-09-25 17:46:20 -0500

    lo veras en tu nube, soy jose editando tu proyecto

---
+++ src/test/sebitex/geraldine/ClassGeraldine.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.sebitex.geraldine;
+
+/**
+ *
+ * @author jose
+ */
+public class ClassGeraldine {
+    public String nombre = "geraldine llanos-esto lo veras en la nube";
+}
